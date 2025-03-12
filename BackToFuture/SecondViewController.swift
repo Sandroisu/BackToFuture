0476573bda8235719b9c25666379304f4448b072
Alex Slatinin <sandroisu1411@gmail.com>
2025-03-12 20:14:28 +0300

    initial commit

---
+++ BackToFuture/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  BackToFuture
+//
+//  Created by Александр Слатинин on 12/03/2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
