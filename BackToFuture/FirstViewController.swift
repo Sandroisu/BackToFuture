4711357d9133d88c1bf791acdd9b26bf8033f63a
Alex Slatinin <sandroisu1411@gmail.com>
2025-03-18 07:02:08 +0300

    added one more animation

--- BackToFuture/FirstViewController.swift
+++ BackToFuture/FirstViewController.swift
@@ -23,6 +23,9 @@ class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+        self.navigationController?.view.backgroundColor = .green
+        timeLabel.text = Utilities().getCurrentTime()
         let year = Utilities().getCurrentYear()
         
         firstLabel.text = Utilities().getLetterAtIndex(text: year, location: 0)
@@ -30,12 +33,19 @@ class FirstViewController: UIViewController {
         thirdLabel.text = Utilities().getLetterAtIndex(text: year, location: 2)
         forthLabel.text = Utilities().getLetterAtIndex(text: year, location: 3)
         
-        self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(FirstViewController.Tick), userInfo: nil, repeats: true)
+        self.timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(FirstViewController.Tick), userInfo: nil, repeats: true)
     }
     
     
     @objc func Tick() {
-        timeLabel.text = Utilities().getCurrentTime()
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseOut,  animations: {
+            self.view.alpha = 0
+        }){(true) in
+            
+            self.view.alpha = 1
+            self.timeLabel.text = Utilities().getCurrentTime()
+        }
     }
 
 
